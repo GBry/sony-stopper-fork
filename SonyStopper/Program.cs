f25102a5acf675d6f51c430dc0499db3d06f0717
tomix89 <84495269+tomix89@users.noreply.github.com>
2022-06-20 14:57:42 +0200

    Bug hunting
    - removing the x86 build
    - adding error reporting

--- SonyStopper/Program.cs
+++ SonyStopper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SonyStopper {
@@ -10,6 +11,19 @@ namespace SonyStopper {
         /// </summary>
         [STAThread]
         static void Main() {
+            
+            if (!System.Diagnostics.Debugger.IsAttached) {
+                // Add the event handler for handling UI thread exceptions to the event.
+                Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+
+                // Set the unhandled exception mode to force all Windows Forms errors
+                // to go through our handler.
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+                // Add the event handler for handling non-UI thread exceptions to the event. 
+                AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            }
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -20,6 +34,15 @@ namespace SonyStopper {
                 // Make sure the application runs!
                 Application.Run();
             }
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e) {
+            MessageBox.Show(e.Exception.Message + '\n' + e.Exception.StackTrace, "Unhandled Thread Exception");
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+            Exception ex = e.ExceptionObject as Exception;
+            MessageBox.Show(ex.Message + '\n' + ex.StackTrace, "Unhandled UI Exception");
         }
     }
 }
